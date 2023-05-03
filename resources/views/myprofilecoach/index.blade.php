5ba72e14fbe9ca5ea7d8a2855747ed83894b3932
Jose Luis Rodriguez <jldsgns@gmail.com>
2023-05-03 09:24:07 -0700

    Changed Workouts for Assigned

--- resources/views/myprofilecoach/index.blade.php
+++ resources/views/myprofilecoach/index.blade.php
@@ -154,7 +154,7 @@
             <div class="modal-dialog modal-lg" style="max-width: 60%;"role="document">
                 <div class="modal-content">
                     <div class="modal-header">
-                        <h5 class="modal-title" id="exampleModalLabel">Workout</h5>
+                        <h5 class="modal-title" id="exampleModalLabel">Assigned</h5>
                         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                             <span aria-hidden="true">&times;</span>
                         </button>
